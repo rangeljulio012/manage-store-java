bc88c743875cf833d47797994bb4ce6f8ad1bc29
Julio_Rangel <julio.rangel@localpayment.com>
2025-04-23 12:08:47 -0400

    Initial project setup with Spring Boot 3.2.5

---
+++ src/test/java/com/manage_store_api/main/MainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manage_store_api.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
