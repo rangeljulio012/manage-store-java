269e488c2105a1eea66c8376f568c68db38f8a15
Julio_Rangel <rangeljulio012@gmail.com>
2025-04-23 16:26:10 -0400

    Ad cors config

---
+++ src/main/java/com/manage_store_api/main/config/CorsConfig.java
@@ -0,0 +1,25 @@
+package com.manage_store_api.main.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("*") // En producción, especifica dominios
+                        .allowedMethods("*")
+                        .allowedHeaders("*")
+                        .exposedHeaders("Authorization") // Headers personalizados
+                        .allowCredentials(false) // true si usas cookies/auth
+                        .maxAge(3600); // Tiempo cache de opciones CORS
+            }
+        };
+    }
+}
